76cec7df3429df5d5439515d1ebcb6d56ae9ce6b
Дудкин Илья Андреевич <dudkin.i@milandr.ru>
2025-10-31 10:58:22 +0300

    use-packages now locates into packages.el file

--- init.el
+++ init.el
@@ -1,98 +1,15 @@
 ;; Enable melpa packages
 (require 'package)
-(load-file "~/.emacs.d/core.el")
-(load-file "~/.emacs.d/keybindings.el")
-(load-file "~/.emacs.d/languages.el")
 (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
 ;; Comment/uncomment this line to enable MELPA Stable if desired.  See `package-archive-priorities`
 ;; and `package-pinned-packages`. Most users will not need or want to do this.
 ;;(add-to-list 'package-archives '("melpa-stable" . "https://stable.melpa.org/packages/") t)
 (package-initialize)
 
-
-(use-package vterm
-  :ensure t
-  :config
-  (define-key vterm-mode-map (kbd "C-S-v") 'vterm-yank)
-  (add-hook 'vterm-mode-hook (lambda () (display-line-numbers-mode -1)))
-  (add-hook 'vterm-mode-hook 'centaur-tabs-local-mode)
-  (define-key vterm-mode-map (kbd "C-d") '(lambda() (interactive) (vterm--self-insert) (delete-window))))
-
-
-(use-package neotree
-  :ensure t
-  :config
-  (global-set-key (kbd "C-b") 'neotree-toggle)
-  (global-set-key (kbd "C-e") 'neotree-show))
-
-
-(use-package eldoc-box
-  :ensure t
-  :config
-  (setq-default eldoc-box-hover-mode t)
-  (setq-default eldoc-box-max-pixel-height 80))
-
-
-(use-package multiple-cursors
-  :ensure t
-  :config
-  (multiple-cursors-mode)
-  (global-set-key (kbd "M-S-<up>") 'mc/mark-previous-like-this)
-  (global-set-key (kbd "M-S-<down>") 'mc/mark-next-like-this))
-
-
-(use-package treemacs
-  :ensure t
-  :config
-  (define-key treemacs-mode-map (kbd "<right>") 'treemacs-RET-action)
-  (define-key treemacs-mode-map (kbd "<left>") 'treemacs-COLLAPSE-action)
-  (global-set-key (kbd "M-o") 'treemacs-select-window)
-  (treemacs-resize-icons 15))
-
-
-(use-package neotree
-  :ensure t
-  :config
-  (setq neo-theme (if (display-graphic-p) 'icons 'arrow))
-  (add-hook 'neotree-mode-hook (lambda() (display-line-numbers-mode -1)))
-  (add-hook 'neo-after-create-hook (lambda (_)(if (display-graphic-p) (call-interactively 'text-scale-once)))))
-
-
-(use-package centaur-tabs
-  :ensure t
-  :config
-  (global-set-key (kbd "M-<up>") 'centaur-tabs-backward-tab)
-  (global-set-key (kbd "M-<down>") 'centaur-tabs-forward-tab))
-
-
-(use-package windmove
-  :ensure t
-  :config
-  (global-set-key (kbd "C-M-<up>") 'windmove-up)
-  (global-set-key (kbd "C-M-<left>") 'windmove-left)
-  (global-set-key (kbd "C-M-<right>") 'windmove-right)
-  (global-set-key (kbd "C-M-<down>") 'windmove-down))
-
-
-(use-package dashboard
-  :ensure t
-  :config
-  (dashboard-setup-startup-hook))
-
-
-(use-package git-gutter
-  :ensure t
-  :config
-  (global-git-gutter-mode))
-
-
-(use-package git-gutter-fringe
-  :ensure t
-  :config
-  (define-fringe-bitmap 'git-gutter-fr:added [224] nil nil '(center repeated))
-  (define-fringe-bitmap 'git-gutter-fr:modified [224] nil nil '(center repeated))
-  (define-fringe-bitmap 'git-gutter-fr:deleted [128 192 224 240] nil nil 'bottom))
-
+(load-file "~/.emacs.d/core.el")
+(load-file "~/.emacs.d/keybindings.el")
+(load-file "~/.emacs.d/languages.el")
+(load-file "~/.emacs.d/packages.el")
 
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
